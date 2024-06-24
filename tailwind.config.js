1d02bd168cc91f568ea1254ec45ca9d5cf8179e5
Sérgio Dantas <sergiodnts828@gmail.com>
2024-06-24 13:35:08 -0300

    build: add and configure shadcn/ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,43 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
   prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
     fontFamily: {
       sans: ['Inria Sans', 'sans-serif'],
     },
-    extend: {},
   },
-  darkMode: 'class',
-  plugins: [],
+  plugins: [tailwindcssAnimate],
 };
